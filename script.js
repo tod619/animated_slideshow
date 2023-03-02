5591d1cbb8705d2bfce16b8a7e2c00141c350ab1
tod619 <trevorodwyer619@hotmail.com>
2023-03-02 12:11:53 +0000

    get slideshow to play in script.js file

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const changeSlides = () => {
     const slideList = document.querySelectorAll('.slide')
     const slides = Array.from(slideList)
 
+    if(current > slides.length){
+        current = 1
+    }
+
     slides.forEach(slide => {
         if(slide.classList[1].split('-')[1]*1 === current) {
             slide.style.cssText = "visibility: visible; opacity: 1"
@@ -14,6 +18,14 @@ const changeSlides = () => {
     })
 }
 
+const playPause = () => {
+    setInterval(()=> {
+        current++;
+        changeSlides()
+    }, 3000)
+}
+
 changeSlides()
+playPause()
 
 
